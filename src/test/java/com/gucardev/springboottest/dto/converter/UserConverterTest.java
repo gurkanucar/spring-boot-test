ce5a7f3eae04089615522d6ae34218a33d6bcfa7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-26 09:55:08 +0300

    test: user converter test

---
+++ src/test/java/com/gucardev/springboottest/dto/converter/UserConverterTest.java
@@ -0,0 +1,49 @@
+package com.gucardev.springboottest.dto.converter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.gucardev.springboottest.dto.UserDTO;
+import com.gucardev.springboottest.dto.request.UserRequest;
+import com.gucardev.springboottest.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserConverterTest {
+
+  private UserConverter userConverter;
+
+  @BeforeEach
+  void setUp() {
+    userConverter = new UserConverter();
+  }
+
+  @Test
+  void mapToEntityTest() {
+    UserRequest userRequest = new UserRequest();
+    userRequest.setUsername("username");
+    userRequest.setEmail("email@test.com");
+    userRequest.setName("Test User");
+
+    User user = userConverter.mapToEntity(userRequest);
+
+    assertEquals(userRequest.getUsername(), user.getUsername());
+    assertEquals(userRequest.getEmail(), user.getEmail());
+    assertEquals(userRequest.getName(), user.getName());
+  }
+
+  @Test
+  void mapToDTOTest() {
+    User user = new User();
+    user.setId(1L);
+    user.setUsername("username");
+    user.setEmail("email@test.com");
+    user.setName("Test User");
+
+    UserDTO userDTO = userConverter.mapToDTO(user);
+
+    assertEquals(user.getId(), userDTO.getId());
+    assertEquals(user.getUsername(), userDTO.getUsername());
+    assertEquals(user.getEmail(), userDTO.getEmail());
+    assertEquals(user.getName(), userDTO.getName());
+  }
+}
