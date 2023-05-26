77fe34ac02fe9e1c19214843c48940a7cbfb11e7
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-26 09:51:08 +0300

    test: user service test

---
+++ src/test/java/com/gucardev/springboottest/service/impl/UserServiceTest.java
@@ -0,0 +1,130 @@
+package com.gucardev.springboottest.service.impl;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+
+import com.gucardev.springboottest.dto.UserDTO;
+import com.gucardev.springboottest.dto.converter.UserConverter;
+import com.gucardev.springboottest.dto.request.UserRequest;
+import com.gucardev.springboottest.model.User;
+import com.gucardev.springboottest.repository.UserRepository;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+public class UserServiceTest extends UserServiceTestSupport {
+
+  @Mock
+  private UserRepository userRepository;
+
+  @Mock
+  private UserConverter userConverter;
+
+  private UserServiceImpl userService;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+    userService = new UserServiceImpl(userRepository, userConverter);
+    super.setupTestData();
+  }
+
+  @Test
+  void findAllTest() {
+    when(userRepository.findAll()).thenReturn(Arrays.asList(user1, user2));
+    when(userConverter.mapToDTO(user1)).thenReturn(userDto1);
+    when(userConverter.mapToDTO(user2)).thenReturn(userDto2);
+
+    List<UserDTO> result = userService.findAll();
+
+    assertEquals(2, result.size());
+  }
+
+  @Test
+  void getByIdTest() {
+    when(userRepository.findById(existingUser.getId())).thenReturn(Optional.of(existingUser));
+
+    User result = userService.getById(existingUser.getId());
+
+    assertEquals(existingUser, result);
+  }
+
+  @Test
+  void getByIdNotFoundTest() {
+    Long nonExistentId = 100L;
+    when(userRepository.findById(nonExistentId)).thenReturn(Optional.empty());
+
+    assertThrows(RuntimeException.class, () -> userService.getById(nonExistentId));
+  }
+
+  @Test
+  void createTest() {
+    when(userRepository.existsByUsernameIgnoreCase(userRequest.getUsername())).thenReturn(false);
+    when(userConverter.mapToEntity(userRequest)).thenReturn(user1);
+    when(userRepository.save(user1)).thenReturn(user1);
+    when(userConverter.mapToDTO(user1)).thenReturn(userDto1);
+
+    UserDTO result = userService.create(userRequest);
+
+    assertEquals(userDto1, result);
+  }
+
+  @Test
+  void createWithExistingUsernameTest() {
+    when(userRepository.existsByUsernameIgnoreCase(userRequest.getUsername())).thenReturn(true);
+
+    assertThrows(RuntimeException.class, () -> userService.create(userRequest));
+  }
+
+  @Test
+  void updateTest() {
+    when(userRepository.existsById(userRequest.getId())).thenReturn(true);
+    when(userRepository.findById(userRequest.getId())).thenReturn(Optional.of(existingUser));
+    when(userConverter.mapToEntity(userRequest)).thenReturn(updatedUser);
+    when(userRepository.save(updatedUser)).thenReturn(updatedUser);
+    when(userConverter.mapToDTO(updatedUser)).thenReturn(updatedUserDto);
+
+    UserDTO result = userService.update(userRequest);
+
+    assertEquals(updatedUserDto, result);
+  }
+
+  @Test
+  void updateNotFoundTest() {
+    Long nonExistentId = 100L;
+    UserRequest nonExistentUserRequest = createUserRequest(nonExistentId, "Non-Existent User", "nonexistent@test.com", "nonExistentUser");
+
+    when(userRepository.existsById(nonExistentId)).thenReturn(false);
+
+    assertThrows(RuntimeException.class, () -> userService.update(nonExistentUserRequest));
+  }
+
+  @Test
+  void deleteTest() {
+    when(userRepository.existsById(existingUser.getId())).thenReturn(true);
+
+    userService.delete(existingUser.getId());
+
+    verify(userRepository).deleteById(existingUser.getId());
+  }
+
+  @Test
+  void deleteNotFoundTest() {
+    Long nonExistentId = 100L;
+    when(userRepository.existsById(nonExistentId)).thenReturn(false);
+
+    assertThrows(RuntimeException.class, () -> userService.delete(nonExistentId));
+    verify(userRepository, never()).deleteById(nonExistentId);
+  }
+}
---
+++ src/test/java/com/gucardev/springboottest/service/impl/UserServiceTestSupport.java
@@ -0,0 +1,54 @@
+package com.gucardev.springboottest.service.impl;
+
+import com.gucardev.springboottest.dto.UserDTO;
+import com.gucardev.springboottest.dto.request.UserRequest;
+import com.gucardev.springboottest.model.User;
+import org.junit.jupiter.api.BeforeEach;
+
+public class UserServiceTestSupport {
+
+  protected User user1, user2, existingUser, updatedUser;
+  protected UserDTO userDto1, userDto2, updatedUserDto;
+  protected UserRequest userRequest;
+
+  void setupTestData() {
+    user1 = createUser("User1", "user1@test.com", "username1");
+    user2 = createUser("User2", "user2@test.com", "username2");
+    userDto1 = createUserDto(user1.getId(), user1.getName(), user1.getEmail(), user1.getUsername());
+    userDto2 = createUserDto(user2.getId(), user2.getName(), user2.getEmail(), user2.getUsername());
+
+    existingUser = createUser("Existing User", "existing@test.com", "existingUser");
+    existingUser.setId(1L);
+    updatedUser = createUser("Updated User", "updated@test.com", "updatedUser");
+    updatedUserDto = createUserDto(updatedUser.getId(), updatedUser.getName(), updatedUser.getEmail(), updatedUser.getUsername());
+
+    userRequest = createUserRequest(1L, "Request User", "request@test.com", "requestUser");
+  }
+
+  protected User createUser(String name, String email, String username) {
+    return User.builder()
+        .name(name)
+        .email(email)
+        .username(username)
+        .build();
+  }
+
+  protected UserDTO createUserDto(Long id, String name, String email, String username) {
+    return UserDTO.builder()
+        .id(id)
+        .name(name)
+        .email(email)
+        .username(username)
+        .build();
+  }
+
+  protected UserRequest createUserRequest(Long id, String name, String email, String username) {
+    UserRequest userRequest = new UserRequest();
+    userRequest.setId(id);
+    userRequest.setName(name);
+    userRequest.setEmail(email);
+    userRequest.setUsername(username);
+    return userRequest;
+  }
+}
+
