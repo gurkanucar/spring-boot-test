bbbd89899118fca0b123acdb4a6b65cfef49d617
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-26 19:05:55 +0300

    feat: feign client added

--- src/main/java/com/gucardev/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/gucardev/springboottest/SpringBootTestApplication.java
@@ -2,8 +2,10 @@ package com.gucardev.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringBootTestApplication {
 
   public static void main(String[] args) {
