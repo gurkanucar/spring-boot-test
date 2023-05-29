f7c8c2244bebf073b9747bc2da374e5670cef992
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-30 01:20:21 +0300

    feat: rateLimiter error handled in GlobalExceptionHandler

--- src/main/java/com/gucardev/springboottest/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gucardev/springboottest/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.gucardev.springboottest.exception;
 
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import java.util.AbstractMap.SimpleEntry;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
   @ExceptionHandler(RuntimeException.class)
   public ResponseEntity<SimpleEntry<String, String>> handleRuntimeException(RuntimeException ex) {
     return new ResponseEntity<>(new SimpleEntry<>("error", ex.getMessage()), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(RequestNotPermitted.class)
+  public ResponseEntity<SimpleEntry<String, String>> handleRateLimitException(
+      RequestNotPermitted e) {
+    return new ResponseEntity<>(
+        new SimpleEntry<>("error", "Rate limit exceeded: " + e.getMessage()),
+        HttpStatus.TOO_MANY_REQUESTS);
   }
 }
