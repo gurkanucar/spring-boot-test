3925c5fa04891293c7abfb8384ca8633e1259f5e
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-05-29 21:16:07 +0300

    test: id field added to user test support

--- src/test/java/com/gucardev/springboottest/service/impl/UserServiceTestSupport.java
+++ src/test/java/com/gucardev/springboottest/service/impl/UserServiceTestSupport.java
@@ -13,16 +13,16 @@ public abstract class UserServiceTestSupport {
   protected UserRequest userRequest;
 
   void setupTestData() {
-    user1 = createUser("User1", "user1@test.com", "user1");
-    user2 = createUser("User2", "user2@test.com", "username2");
-    user3 = createUser("User3", "user3@test.com", "username3");
+    user1 = createUser(1L, "User1", "user1@test.com", "user1");
+    user2 = createUser(2L, "User2", "user2@test.com", "username2");
+    user3 = createUser(3L, "User3", "user3@test.com", "username3");
     userDto1 = createUserDto(user1.getId(), user1.getName(), user1.getEmail(), user1.getUsername());
     userDto2 = createUserDto(user2.getId(), user2.getName(), user2.getEmail(), user2.getUsername());
     userDto3 = createUserDto(user3.getId(), user3.getName(), user3.getEmail(), user3.getUsername());
 
-    existingUser = createUser("Existing User", "existing@test.com", "existingUser");
+    existingUser = createUser(1L, "Existing User", "existing@test.com", "existingUser");
     existingUser.setId(1L);
-    updatedUser = createUser("Updated User", "updated@test.com", user1.getUsername());
+    updatedUser = createUser(1L, "Updated User", "updated@test.com", user1.getUsername());
     updatedUserDto =
         createUserDto(
             updatedUser.getId(),
@@ -33,8 +33,8 @@ public abstract class UserServiceTestSupport {
     userRequest = createUserRequest(1L, "Request User", "request@test.com", "requestUser");
   }
 
-  protected User createUser(String name, String email, String username) {
-    return User.builder().name(name).email(email).username(username).build();
+  protected User createUser(Long id, String name, String email, String username) {
+    return User.builder().id(id).name(name).email(email).username(username).build();
   }
 
   protected UserDTO createUserDto(Long id, String name, String email, String username) {
